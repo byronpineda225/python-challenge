5ad1234a64c126285f3ddd5a383b0156e2364d67
byronpineda225 <84112237+byronpineda225@users.noreply.github.com>
2021-06-19 16:14:38 -0400

    Added latest changes to main.py

--- PyBank/main.py
+++ PyBank/main.py
@@ -6,21 +6,90 @@ import os
 import csv
 
 #csvpath = os.path.join('E:/python-challenge/PyBank/Resources/budget_data.csv')
-csvpath = os.path.join('Resources', 'budget_data.csv')
+csvpath = os.path.join("Resources","budget_data.csv")
+
+line_counter = []
+months_counter = 0
+net_total = 0
+prior_months_total = 0
+current_months_total = 0
+net_changes = []
+monthly_changes = []
+average_changes = 0.00
+net_delta = 0
 
 # Method 2: Improved Reading using CSV module
 
 with open(csvpath) as csvfile:
-
+     
     # CSV reader specifies delimiter and variable that holds contents
     csvreader = csv.reader(csvfile, delimiter=',')
 
-    print(csvreader)
+    # Skip the header
+    next(csvreader)
 
-    # Read the header row first (skip this step if there is now header)
-    csv_header = next(csvreader)
-    print(f"CSV Header: {csv_header}")
-
-    # Read each row of data after the header
     for row in csvreader:
-        print(row)
+       
+        # Total months counter
+        months_counter += 1
+
+        # The net total amount of "Profit/Losses" over the entire period
+        net_total += int(row[1]) 
+      
+        # Calculate the average change in Profits/Losses over the entire period
+        net_delta = int(row[1]) - prior_months_total
+        prior_months_total = int(row[1])
+        net_changes.append(net_delta)
+        monthly_changes.append(row[0])
+
+# average_changes 
+average_changes = round(sum(net_changes)/months_counter,2)     
+          
+print("Financial Analysis")
+print("----------------------------")
+print(f'Total Months: {months_counter}')
+print(f'Total: {net_total}')
+print(f'Average Change: {average_changes}')
+
+# Zip lists together
+cleaned_csv = zip(net_changes, monthly_changes) 
+
+# Get the maximum net changes and its associated da
+xmax =  cleaned_csv
+
+xmax = list(max(xmax))
+print(f'Greatest Increase in Profits: {xmax[1]} (${xmax[0]})') 
+
+cleaned_csv = zip(net_changes, monthly_changes) 
+
+xmin = cleaned_csv
+
+xmin = list(min(xmin))
+print(f'Greatest Decrease in Profits: {xmin[1]} (${xmin[0]})') 
+
+cleaned_csv = zip(net_changes, monthly_changes) 
+
+# Set variable for output file
+
+output_file = os.path.join("analysis","budget_final_analysis.txt")
+
+#  Open the output file
+with open(output_file, "w") as datafile:
+    writer = csv.writer(datafile)
+
+    # Write in zipped rows
+    # writer.writerows(cleaned_csv)
+    datafile.write("Financial Anaylsis\n") 
+    datafile.write("----------------------------\n")
+    datafile.write(f'Total Months: {months_counter}\n')
+    datafile.write(f'Total: {net_total}\n')
+    datafile.write(f'Average Change: {average_changes}\n')
+    datafile.write(f'Greatest Increase in Profits: {xmax[1]} (${xmax[0]})\n')  
+    datafile.write(f'Greatest Decrease in Profits: {xmin[1]} (${xmin[0]})\n') 
+
+
+
+
+
+
+
